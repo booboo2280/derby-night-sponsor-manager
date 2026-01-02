6f4f38389da76e5a171b95feb993afaa838068d1
booboo2280 <booboo2280@gmail.com>
2026-01-02 14:18:49 -0500

    nav 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ export default function App() {
   return (
     <Router>
       <nav className="bg-slate-800 text-white p-4 flex gap-12">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/sponsors" className="hover:underline">Sponsorship Manager</Link>
-        <Link to="/decorations" className="hover:underline">Decorations</Link>
+        <Link to="/" className="hover:underline" style={{ marginRight: "1.5rem" }}>Home</Link>
+        <Link to="/sponsors" className="hover:underline" style={{ marginRight: "1.5rem" }}>Sponsorship Manager</Link>
+        <Link to="/decorations" className="hover:underline" style={{ marginRight: "1.5rem" }}>Decorations</Link>
       </nav>
 
       <div className="p-6">
